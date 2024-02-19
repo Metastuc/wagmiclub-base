34ec90568e798982f2966535868d68b6630145bc
sinachi ajuzie <Ajuziesinachi@gmail.com>
2024-02-19 01:11:40 +0100

    ongoing: migration from web3modal and ethers.js to rainbowkit and wagmi (with ethers.js adapters)

---
+++ client/src/hooks/rainbowKit.js
@@ -0,0 +1,56 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import { http } from 'wagmi';
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  rainbowWallet,
+  walletConnectWallet,
+  bitgetWallet,
+  metaMaskWallet,
+  trustWallet
+} from '@rainbow-me/rainbowkit/wallets';
+import { createConfig } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import {
+  base,
+} from 'wagmi/chains';
+
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [metaMaskWallet, walletConnectWallet, bitgetWallet, trustWallet],
+    },
+  ],
+  {
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+  }
+);
+
+const config = createConfig({
+  chains: [mainnet, sepolia],
+  connectors,
+  ssr: true, 
+  transports: {
+    [base.id]: http('https://mainnet.example.com'),
+  },
+})
+
+const client = new QueryClient();
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider>
+            <Component {...pageProps} />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default MyApp;
